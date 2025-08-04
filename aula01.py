2e4ca944f1f57ced4be22d9d295c2a355cfc85b1
Arc23101 <antonyvb@yahoo.com.br>
2025-08-04 11:55:17 -0300

    Add files via upload

--- aula01.py
+++ aula01.py
@@ -15,4 +15,43 @@ Peso = float(input())
 print ("Digite sua altura:")
 Altura = float(input())
 Imc = str(Peso/(Altura * Altura))
-print ("Imc: " + Imc)
+print ( Nome + " seu IMC é:", Imc )
+
+print ("Digite qual ano estamos:")
+NossoAno = (input())
+print ("Digite seu ano de nascimento:")
+NascimentoData = (input())
+Idade =str(NascimentoData - NascimentoData)
+print (Idade)
+
+print ("quantos dias você ficou com o carro")
+Dias = input()
+ValorSerPago = (Dias * 100)
+print ( ValorSerPago)
+
+
+print ("temp atual")
+TempAtual = input()
+TempEmFarhenheit = (TempAtual * 9/5) + 32
+
+
+
+
+print ("Digite suas notas")
+print ("Nota 1")
+Nota1 = input()
+print ("Nota 2")
+Nota2 = input()
+print ("Nota 3")
+Nota3 = input()
+print ("Nota 4")
+Nota4 = input()
+MediaSua = (Nota1 + Nota2 + Nota3 + Nota4)/4
+print (MediaSua)
+
+
+IdadeMeses = Idade * 12
+print ("sua idade é: " +IdadeMeses)
+
+
+

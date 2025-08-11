6a42fc4ca6a56d038a31eabaf3f69c116eab965e
Arc23101 <antonyvb@yahoo.com.br>
2025-08-11 09:13:13 -0300

    Add files via upload

--- TDE 1 Raciocínio algorítmico.py
+++ TDE 1 Raciocínio algorítmico.py
@@ -2,40 +2,32 @@ print("Digite um numero: ")
 Num1 = int(input())
 Num2 = (Num1 - 1)
 Num3 = (Num1 + 1)
-print ("anterior = " + F"{Num2}", "Seu numero = " + F"{Num1}", "Próximo numero = " + F"{Num3}")
+print (f"anterior = {Num2}, Seu numero = {Num1}, Próximo numero =  {Num3}")
 
-print ("Digite seu ano de nascimento: ")
-Nascimento = int(input())
-print ("Digite qual ano estamos: ")
-NossoAno = int(input())
+Nascimento = int(input("Digite seu ano de nascimento: "))
+NossoAno = int(input("Digite qual ano estamos: "))
 Idade = NossoAno - Nascimento
 print ("Sua idade até o final do ano é: " + f"{Idade}")
 
-print ("Digite seu salário professor: ")
-Salario = int(input())
+Salario = int(input("Digite seu salário professor: "))
 SalariosMinímos = Salario/1412
 print ("Seu salário em salários minímos de 2024 é: " + f"{SalariosMinímos}")
 
-print("Digite o valor do produto: ")
-vProduto = int(input())
+vProduto = int(input("Digite o valor do produto: "))
 vVista = (vProduto - (vProduto/20))
 vDuasVezes = (vProduto/2)
 vTresVezes = ((vProduto + (vProduto/20))/3)
 print("A vista o valor é: " + f"{vVista}", "O valor em 2 parcelas é: " + f"{vDuasVezes}", "cada. E o valor em 3 vezes é: " + f"{vTresVezes}" + "cada parcela")
 
-print("Quanto KM você andou? ")
-Km = int(input())
-print("Quantos litros foram consumidos? ")
-Litros = int(input())
+Km = int(input("Quanto KM você andou? "))
+Litros = int(input("Quantos litros foram consumidos? "))
 Media = (Km/Litros)
 print("Sua média é de: " + f"{Media}")
 
 import math 
 vLata = 50
-print("Qual o raio do cilindro(metros): ")
-Raio = float(input())
-print("Qual a altura(metros): ")
-Altura = float(input())
+Raio = float(input("Qual o raio do cilindro(metros): "))
+Altura = float(input("Qual a altura(metros): "))
 areaEmMetros = ((3.14 * (Raio * Raio) * 2) + (2 * 3.14 * Raio * Altura))
 LitrosLata = 5
 LitroMetro = 3

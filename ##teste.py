bb6a5801d2bf8c4dd7a4841548eabdb14447aa81
Arc23101 <antonyvb@yahoo.com.br>
2025-10-24 10:11:35 -0300

    Add files via upload

---
+++ ##teste.py
@@ -0,0 +1,11 @@
+##teste
+##pip install truth-table-generator
+'''
+import ttg 
+print(ttg.Truths(['p','q'], [('(p and q) => (p or q)')], ints=False))
+'''
+b = [1,2,3,4,5]
+i = 0
+while (i<len(b)):
+    print(b[i], end='<')
+    i += 1

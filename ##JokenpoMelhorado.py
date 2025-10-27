7ab4f4c04e747b41d859145daa7113c067fa61a5
Arc23101 <antonyvb@yahoo.com.br>
2025-10-27 10:47:30 -0300

    Add files via upload

--- ##JokenpoMelhorado.py
+++ ##JokenpoMelhorado.py
@@ -25,6 +25,35 @@ def verificarGanhador(escolhaPlayer1, escolhaPlayer2):
             return 2
     else:
         return 1
+def Jogo():
+    global vPlayer1,vPlayer2, continuar
+    if modalidade == 1:
+            escolhaPlayer1 = coletarJogadaHumano()
+            escolhaPlayer2 = coletarJogadaHumano()
+    elif modalidade == 2:
+            escolhaPlayer1 = coletarJogadaHumano()
+            escolhaPlayer2 = coletarJogadaBot()
+            print(f"Player 2 escolheu {escolhaPlayer2}")
+    elif modalidade == 3:
+            escolhaPlayer1 = coletarJogadaBot()
+            escolhaPlayer2 = coletarJogadaBot()
+            print(f"Player 1 escolheu {escolhaPlayer1} \nE o Player 2 escolheu {escolhaPlayer2}")
+    resultado = verificarGanhador(escolhaPlayer1,escolhaPlayer2)
+    if resultado == 0 :
+            print(f"Empate \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
+    elif resultado == 1:
+            vPlayer1 += 1
+            print(f"Player 1 ganhou \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
+    else:
+            vPlayer2 += 1
+            print(f"Player 2 ganhou \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
+    continuar = int(input("Gostaria de continuar? se sim digite 1, se não digite 0: "))
+    if continuar < 0 or continuar > 1:
+            continuar = int(input("Numero inválido digite Novamente se sim digite 1, se não digite 0: "))
+    if  continuar == 1:
+        Jogo()
+    else:
+        return 0
     
 nomeP1 = "Player 1"
 escolhaPlayer1 = 0
@@ -41,29 +70,6 @@ if modalidade < 3:
     nomeP1 = input("Digite o nome do player 1: ")
     if modalidade < 2:
         nomeP2 = input("Digite o nome do player 2: ")
-        
-while continuar == 1:
-    if modalidade == 1:
-        escolhaPlayer1 = coletarJogadaHumano()
-        escolhaPlayer2 = coletarJogadaHumano()
-    elif modalidade == 2:
-        escolhaPlayer1 = coletarJogadaHumano()
-        escolhaPlayer2 = coletarJogadaBot()
-        print(f"Player 2 escolheu {escolhaPlayer2}")
-    elif modalidade == 3:
-        escolhaPlayer1 = coletarJogadaBot()
-        escolhaPlayer2 = coletarJogadaBot()
-        print(f"Player 1 escolheu {escolhaPlayer1} \nE o Player 2 escolheu {escolhaPlayer2}")
-    resultado = verificarGanhador(escolhaPlayer1,escolhaPlayer2)
-    if resultado == 0 :
-        print(f"Empate \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
-    elif resultado == 1:
-        vPlayer1 += 1
-        print(f"Player 1 ganhou \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
-    else:
-        vPlayer2 += 1
-        print(f"Player 2 ganhou \naqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} ")
-    continuar = int(input("Gostaria de continuar? se sim digite 1, se não digite 0: "))
-    if continuar < 0 or continuar > 1:
-        continuar = int(input("Numero inválido digite Novamente se sim digite 1, se não digite 0: "))
+Jogo()
+
 print(f"aqui esta o placar Geral: \n{nomeP1} ganhou {vPlayer1} \n{nomeP2} ganhou {vPlayer2} \nObrigado por jogar")
